debacf7c9b697579773324fccef4d6c5cd062aa5
sibirajen <sibirajen16@gmail.com>
2024-12-16 17:49:35 +0530

    "Changed object name to tree"

--- DSA/src/binarySearchTree/Main.java
+++ DSA/src/binarySearchTree/Main.java
@@ -4,24 +4,24 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		BinarySearchTree BST = new BinarySearchTree();
-		BST.insert(50);
-		BST.insert(30);
-		BST.insert(70);
-		BST.insert(20);
-		BST.insert(40);
-		BST.insert(60);
-		BST.insert(80);
+		BinarySearchTree tree = new BinarySearchTree();
+		tree.insert(50);
+		tree.insert(30);
+		tree.insert(70);
+		tree.insert(20);
+		tree.insert(40);
+		tree.insert(60);
+		tree.insert(80);
 		
-		BST.printTree();
-		BST.delete(20);
-		BST.printTree();
-		BST.delete(70);
-		BST.printTree();
-		BST.delete(50);
-		BST.printTree();
+		tree.printTree();
+		tree.delete(20);
+		tree.printTree();
+		tree.delete(70);
+		tree.printTree();
+		tree.delete(50);
+		tree.printTree();
 		
-		BST.find(60);
-		BST.find(10);
+		tree.find(60);
+		tree.find(10);
 	}
 }
